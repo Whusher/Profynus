72b103a65e78c856d15598888dc4ff478a3bcc20
Angel Anaya <eduarwwhusher@gmail.com>
2026-03-15 04:36:01 -0600

    Prod Test

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ const MusicPlayerPage = lazy(() => import('@pages/music-player/MusicPlayerPage')
 const renderLazyRoute = (element) => <Suspense fallback={<RouteLoader />}>{element}</Suspense>
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='./'>
     <Routes>
       {/* Public paths */}
       <Route path='/' element={renderLazyRoute(<LandingPage/>)} />
